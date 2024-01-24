b21afb0b07a03f77d58b32cd8cf0155493cba137
GameMutPlay14 <77333367+Savin1play140@users.noreply.github.com>
2024-01-24 11:01:26 +0300

    Add files via upload

--- RM.php
+++ RM.php
@@ -2,7 +2,10 @@
 $filename = "./version.txt";
 $file = fopen($filename, "r");
 if (!is_null(@$_GET['get-last-version'])) {
-	echo fread($file, filesize($filename)-2);
+	echo fread($file, filesize($filename)-5);
+} elseif (!is_null(@$_GET['get-last-build'])) {
+	$verData = explode("_", fread($file, filesize($filename)), 2);
+	echo $verData[1];
 } elseif (!is_null(@$_GET['get-update'])) {
 	header("Location: ./Releases/".fread($file, filesize($filename)).".zip");
 }
